fff1313729b33f2d6d630465c28fed5ff64d3d33
Benjamin Hudgens <benjamin@benjamindavid.com>
2015-03-28 01:03:59 -0500

    Patch that works

--- vmware_hgfs_fix.sh
+++ vmware_hgfs_fix.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
-cd /usr/lib/vmware-tools/modules/source
-tar xf vmhgfs.tar
-grep -q d_u.d_alias vmhgfs-only/inode.c && echo "already patched" && exit 0
-sed -i -e s/d_alias/d_u.d_alias/ vmhgfs-only/inode.c
-cp -p vmhgfs.tar vmhgfs.tar.orig
-tar cf vmhgfs.tar vmhgfs-only
-vmware-config-tools.pl -d -m
+cd /tmp
+sudo apt-get install -y unzip
+git clone https://github.com/rasa/vmware-tools-patches.git
+cd vmware-tools-patches
+sudo ./download-tools.sh 7.1.1
+sudo ./untar-and-patch.sh
+sudo ./compile.sh
