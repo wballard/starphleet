536278ca3aad16ec309b801226757ff63070b139
Amit Segal <asegal@gmail.com>
2015-11-17 00:01:05 -0500

    pass jwt secret env to lua script

--- nginx/lua/guard.lua
+++ nginx/lua/guard.lua
@@ -1,12 +1,13 @@
 local jwt = require "resty.jwt"
 local jwt_token = ngx.var.arg_jwt
+local jwt_secret = os.getenv("JWT_SECRET")
 if jwt_token then
     ngx.header['Set-Cookie'] = "jwt=" .. jwt_token
 else
     jwt_token = ngx.var.cookie_jwt
 end
 
-local jwt_obj = jwt:verify(ngx.var.jwt_secret, jwt_token, 0)
+local jwt_obj = jwt:verify(jwt_secret, jwt_token, 0)
 
 if not jwt_obj["verified"] then
     local site = ngx.var.scheme .. "://" .. ngx.var.http_host;
@@ -14,6 +15,14 @@ if not jwt_obj["verified"] then
 
     ngx.status = ngx.HTTP_UNAUTHORIZED
     ngx.say("unauthorized - no valid token found");
+    local jwt_token = jwt:sign(
+      jwt_secret,
+      {
+        header={typ="JWT", alg="HS256"},
+        payload={foo="bar"}
+      }
+    )
+    ngx.say(jwt_token)
     ngx.exit(ngx.HTTP_OK)
 
     -- or you can redirect to your website to get a new jwt token
