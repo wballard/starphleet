a67625a5e85af9b5bca7af8754782bd0b1ed80bf
Amit Segal <asegal@gmail.com>
2015-11-24 12:45:56 -0500

    correctly handle jwt querystring and cookie by precedence, log error message for missing env values, trim roles before checking

--- nginx/lua/guard.lua
+++ nginx/lua/guard.lua
@@ -2,29 +2,38 @@ require "os"
 require "string"
 require "table"
 local jwt = require "resty.jwt"
-local jwt_token = ngx.var.arg_jwt or ngx.var.cookie_jwt
 local jwt_secret = os.getenv("JWT_SECRET")
 local jwt_auth_site = os.getenv("JWT_AUTH_SITE")
 local jwt_roles = {}
 local role_authorized = false
 
---TODO: we need to return a 500 with a message useful for developers
 if not jwt_auth_site then
-  jwt_auth_site = "/"
+  ngx.log(ngx.ERR, "Error processing jwt authentication. Missing JWT_AUTH_SITE environment variable")
+  return ngx.exit(500)
+end
+
+if not jwt_secret then
+  ngx.log(ngx.ERR, "Error processing jwt authentication. Missing JWT_SECRET environment variable")
+  return ngx.exit(500)
+end
+
+function trim (s)
+  return (string.gsub(s, "^%s*(.-)%s*$", "%1"))
 end
 
 for role in string.gmatch(ngx.var.jwt_roles, '([^,]+)') do
   table.insert(jwt_roles,1,role)
 end
 
-local jwt_obj = jwt:verify(jwt_secret, jwt_token, 0)
+local jwt_obj = jwt:verify(jwt_secret, ngx.var.arg_jwt, 0)
+if not jwt_obj["verified"] then
+  jwt_obj = jwt:verify(jwt_secret, ngx.var.cookie_jwt, 0)
+end
 
 if type(jwt_obj.payload) == "table" then
-  -- TODO: need to trim strings
-  -- TODO: changed "role" to "roles", after we've updated the token creator
   for user_role in string.gmatch(jwt_obj.payload.role, '([^,]+)') do
     for _,v in pairs(jwt_roles) do
-      if v == user_role or v == '*' then
+      if trim(v) == trim(user_role) or trim(v) == '*' then
         role_authorized = true
         break
       end
--- nginx/lua/jwt_cookie.lua
+++ nginx/lua/jwt_cookie.lua
@@ -1,14 +1,17 @@
 require "os"
 local jwt = require "resty.jwt"
 local jwt_secret = os.getenv("JWT_SECRET")
-local jwt_token = ngx.var.arg_jwt or ngx.var.cookie_jwt
 local token_duration = 60
 
-local jwt_obj = jwt:verify(jwt_secret, jwt_token, 0)
+local jwt_obj = jwt:verify(jwt_secret, ngx.var.arg_jwt, 0)
+if not jwt_obj["verified"] then
+  jwt_obj = jwt:verify(jwt_secret, ngx.var.cookie_jwt, 0)
+end
+
 if jwt_obj["verified"] then
   local exp = ngx.time() + token_duration
   local token = { payload=jwt_obj.payload, header= { typ="JWT", alg="HS256"} }
   token.payload.exp = exp
   local jwt_cookie = jwt:sign(jwt_secret,token)
-  ngx.header['Set-Cookie'] = "jwt=" .. jwt_cookie .. "; Path=/; Expires=" .. ngx.cookie_time(exp)
+  ngx.header['Set-Cookie'] = "jwt=" .. jwt_cookie .. "; Path=/; Expires=" .. ngx.cookie_time(ngx.time() + token_duration)
 end
