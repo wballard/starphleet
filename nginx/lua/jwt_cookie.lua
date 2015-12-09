94b062d6f724b1f317089a5b90f0e6d0091bd873
asegal / squince <asegal@glgroup.com , squince@glgroup.com>
2015-12-09 16:35:31 -0500

    set Domain for JWT cookie explicitly

--- nginx/lua/jwt_cookie.lua
+++ nginx/lua/jwt_cookie.lua
@@ -1,6 +1,7 @@
 require "os"
 local jwt = require "resty.jwt"
 local jwt_secret = os.getenv("JWT_SECRET")
+local jwt_cookie_domain = os.getenv("JWT_COOKIE_DOMAIN")
 local token_duration = 900
 
 local jwt_obj = jwt:verify(jwt_secret, ngx.var.arg_jwt, 0)
@@ -13,5 +14,5 @@ if jwt_obj["verified"] then
   local token = { payload=jwt_obj.payload, header= jwt_obj.header }
   token.payload.exp = exp
   local jwt_cookie = jwt:sign(jwt_secret,token)
-  ngx.header['Set-Cookie'] = "jwt=" .. jwt_cookie .. "; Path=/; Expires=" .. ngx.cookie_time(ngx.time() + token_duration)
+  ngx.header['Set-Cookie'] = "jwt=" .. jwt_cookie .. "; Domain=" .. jwt_cookie_domain .. "; Path=/; Expires=" .. ngx.cookie_time(ngx.time() + token_duration)
 end
