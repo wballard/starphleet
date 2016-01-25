d26b740a6c54628c232015aedbf71c32186ee50c
Amit Segal <asegal@gmail.com>
2016-01-25 16:45:57 -0500

    only sign a new token when the old token has expired, but is withing the leeway

--- nginx/lua/guard.lua
+++ nginx/lua/guard.lua
@@ -6,6 +6,7 @@ local jwt_secret = os.getenv("JWT_SECRET")
 local jwt_auth_site = os.getenv("JWT_AUTH_SITE")
 local jwt_roles = {}
 local role_authorized = false
+local leeway = 900
 
 if not jwt_auth_site then
   ngx.log(ngx.ERR, "Error processing jwt authentication. Missing JWT_AUTH_SITE environment variable")
@@ -27,12 +28,12 @@ end
 
 -- jwt:verify checks that the token can be decrypted,
 -- is salted with the specified secret, and has not expired
-local jwt_obj = jwt:verify(jwt_secret, ngx.var.arg_jwt, 0)
+local jwt_obj = jwt:verify(jwt_secret, ngx.var.arg_jwt, 900)
 if not jwt_obj["verified"] then
-  jwt_obj = jwt:verify(jwt_secret, ngx.var.cookie_jwt, 0)
+  jwt_obj = jwt:verify(jwt_secret, ngx.var.cookie_jwt, 900)
 end
 
-if type(jwt_obj.payload) == "table" then
+if jwt_obj["verified"] and type(jwt_obj.payload) == "table" then
   -- check that there are exp and iat properties
   -- and that iat is not more than 24 hours old
   if jwt_obj.payload.exp and jwt_obj.payload.iat and ngx.time() - jwt_obj.payload.iat <= 24*60*60 then
@@ -63,7 +64,10 @@ if jwt_obj["verified"] and role_authorized then
   for k,v in pairs(jwt_obj.payload) do
     ngx.req.set_header("jwt-" .. k, v)
   end
-  ngx.ctx.jwt_obj = jwt_obj
+  local ctx = ngx.ctx
+  ctx.jwt_obj = jwt_obj
+  ctx.leeway = leeway
+  ngx.ctx = ctx
 else
   local full_request_uri = ngx.var.scheme .. '://' .. ngx.var.host .. ngx.var.request_uri
   return ngx.redirect(jwt_auth_site .. "?target=" ..ngx.escape_uri(full_request_uri) )
--- nginx/lua/jwt_cookie.lua
+++ nginx/lua/jwt_cookie.lua
@@ -1,16 +1,24 @@
-require "os"
-local jwt = require "resty.jwt"
-local jwt_secret = os.getenv("JWT_SECRET")
-local jwt_cookie_domain = os.getenv("JWT_COOKIE_DOMAIN")
-local token_duration = os.getenv("JWT_EXPIRATION") or 3600
 -- grab the already parsed and verified jwt_obj from the ngx per request cache
-local jwt_obj = ngx.ctx.jwt_obj
+local ctx = ngx.ctx
+local jwt_obj = ctx.jwt_obj
 
 if jwt_obj and jwt_obj["verified"] then
-  local exp = ngx.time() + token_duration
-  local token = { payload=jwt_obj.payload, header= jwt_obj.header }
-  token.payload.exp = exp
-  local jwt_cookie = jwt:sign(jwt_secret,token)
-  local cookie_domain = not jwt_cookie_domain and '' or '; Domain=' .. jwt_cookie_domain
-  ngx.header['Set-Cookie'] = "jwt=" .. jwt_cookie .. cookie_domain .. "; Path=/; Expires=" .. ngx.cookie_time(ngx.time() + token_duration)
+  require "os"
+  local jwt = require "resty.jwt"
+  local jwt_secret = os.getenv("JWT_SECRET")
+  local jwt_cookie_domain = os.getenv("JWT_COOKIE_DOMAIN")
+  local token_duration = os.getenv("JWT_EXPIRATION") or 3600
+  local payload = jwt_obj["payload"]
+  local leeway = ctx.leeway
+  -- if the token is verified but has expired, it must be within the leeway, so reissue a new token
+  if not payload["session"] or (payload["exp"] and type(payload["exp"]) == "number" and payload["exp"] < ngx.now() ) then
+    if session then ngx.log(ngx.INFO, 'session') end
+    local duration = payload["session"] and leeway or token_duration
+    local exp = ngx.time() + duration
+    payload["session"] = 'true'
+    payload["exp"] = exp
+    local jwt_cookie = jwt:sign(jwt_secret, { payload=payload, header=jwt_obj.header } )
+    local cookie_domain = not jwt_cookie_domain and '' or '; Domain=' .. jwt_cookie_domain
+    ngx.header['Set-Cookie'] = "jwt=" .. jwt_cookie .. cookie_domain .. "; Path=/; Expires=" .. ngx.cookie_time(ngx.time() + token_duration + leeway)
+  end
 end
