c61ec3460bf7595cdf7fcb8ba35da070d04e94f6
Benjamin Hudgens <benjamin@benjamindavid.com>
2016-03-22 09:04:30 -0500

    Patch for latest vmware version

--- vmware_hgfs_fix.sh
+++ vmware_hgfs_fix.sh
@@ -3,5 +3,5 @@ cd /tmp
 sudo apt-get install -y unzip git
 git clone https://github.com/rasa/vmware-tools-patches.git
 cd vmware-tools-patches
-sudo ./download-tools.sh 7.1.1
+sudo ./download-tools.sh 8.1.0
 sudo ./untar-and-patch-and-compile.sh
